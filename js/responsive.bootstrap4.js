d574349a52c79f6c2bf1295972c0bb573be33371
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-16 17:11:35 +0000

    89b36ae8eb924baa2ef7aa515684764a7edbfc12
    Sync to source repo @9b36ae8eb924baa2ef7aa515684764a7edbfc12

--- js/responsive.bootstrap4.js
+++ js/responsive.bootstrap4.js
@@ -28,10 +28,9 @@
 				require('datatables.net-bs4')(root, $);
 			}
 
-			if ( ! $.fn.dataTable ) {
+			if ( ! $.fn.dataTable.Responsive ) {
 				require('datatables.net-responsive')(root, $);
 			}
-
 
 			return factory( $, root, root.document );
 		};
--- js/responsive.bootstrap4.mjs
+++ js/responsive.bootstrap4.mjs
@@ -4,7 +4,7 @@
 
 import $ from 'jquery';
 import DataTable from 'datatables.net-bs4';
-import 'datatables.net-responsive';
+import Responsive from 'datatables.net-responsive';
 
 
 
