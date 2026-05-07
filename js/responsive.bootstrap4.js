9e285e1a0f4d97c0d9db23e76458b77c630c09c7
Allan Jardine <allan@datatables.net>
2026-05-07 15:36:46 +0000

    Updating package for DataTables 3

--- js/responsive.bootstrap4.js
+++ js/responsive.bootstrap4.js
@@ -1,7 +1,54 @@
-/*! Bootstrap 4 integration for DataTables' Responsive
- * © SpryMedia Ltd - datatables.net/license
+/*! Responsive Bootstrap 4 styling 4.0.0-beta.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
+(function(factory){
+	if (typeof define === 'function' && define.amd) {
+		// AMD
+		define(['datatables.net-bs4', 'datatables.net-responsive'], function (dt) {
+			return factory(window, document, dt);
+		});
+	}
+	else if (typeof exports === 'object') {
+		// CommonJS
+		var cjsRequires = function (root) {
+			if (! root.DataTable) {
+				require('datatables.net-bs4')(root);
+			}
+
+			if (! window.DataTable.Responsive) {
+				require('datatables.net-responsive')(root);
+			}
+		};
+
+		if (typeof window === 'undefined') {
+			module.exports = function (root) {
+				if (! root) {
+					// CommonJS environments without a window global must pass a
+					// root. This will give an error otherwise
+					root = window;
+				}
+
+				cjsRequires(root);
+				return factory(root, root.document, root.DataTable);
+			};
+		}
+		else {
+			cjsRequires(window);
+			module.exports = factory(window, window.document, window.DataTable);
+		}
+	}
+	else {
+		// Browser
+		factory(window, document, window.DataTable);
+	}
+}(function(window, document, DataTable) {
+'use strict';
+
+
+
+// Note that BS4's JS depends upon jQuery, so we use it here
+var $ = DataTable.use('jq');
 var _display = DataTable.Responsive.display;
 var _original = _display.modal;
 var _modal = $(
@@ -62,3 +109,7 @@ _display.modal = function (options) {
 		}
 	};
 };
+
+
+return DataTable;
+}));
