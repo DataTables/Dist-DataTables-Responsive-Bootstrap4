f1b2a52ec0a271ca88bc3086431c26309f4ca152
dtbuild <dtbuild@rigil>
2026-07-22 14:41:46 +0000

    e4eadd8925633dcb5da1f60d364e8a0026a9e45a Release 4.0.0 ready
    Sync to source repo @e4eadd8925633dcb5da1f60d364e8a0026a9e45a

--- js/responsive.bootstrap4.js
+++ js/responsive.bootstrap4.js
@@ -1,4 +1,4 @@
-/*! Responsive Bootstrap 4 styling 4.0.0-beta.1 for DataTables
+/*! Responsive Bootstrap 4 styling 4.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
